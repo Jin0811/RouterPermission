d08744295cbc9c5887c644e2306630287de29125
Jin0811 <15797954826@163.com>
2022-04-13 21:33:49 +0800

    修复路由跳转时，正常跳转但是报错的问题

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -9,4 +9,13 @@ const router = new VueRouter({
   routes,
 });
 
+// 修复路由跳转时，正常跳转但是报错的问题
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject) {
+    return originalPush.call(this, location, onResolve, onReject);
+  }
+  return originalPush.call(this, location).catch((err) => err);
+};
+
 export default router;
